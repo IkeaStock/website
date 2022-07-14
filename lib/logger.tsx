635e5f54affd682d525912485b26d89225b36382
devramsean0 <outramsean0@gmail.com>
2022-07-14 11:49:48 +0000

    .H.

--- lib/logger.tsx
+++
@@ -1,9 +0,0 @@
-class logger {
-    public debug(message: any, debugEnabled: string): void {
-        if (debugEnabled == "true") {
-            console.log(message);
-            return
-        } else return
-    }
-}
-export default new logger();
