1613ee7f9fba77cd013bb83ff1423be6492d26f4
devramsean0 <outramsean0@gmail.com>
2022-07-21 12:13:21 +0000

    analytics

--- pages/api/stores/[product].tsx
+++ pages/api/stores/[product].tsx
@@ -1,12 +1,11 @@
 import { IkeaApi } from "@ikeastock/api";
+import analytics from "../../../lib/analytics";
 import { apiDocsENV } from "../../../lib/apiDocsENV";
-import logger from "../../../lib/logger";
 const ikea = new IkeaApi();
 export default async function handler(req, res) {
   if (apiDocsENV("API_ENABLED") == "true") {
   const { product, country, debug } = req.query;
     const initData = await ikea.get(country, product);
-    logger.debug(initData , debug)
     const dataArr = []
     initData.map(async (val) => {
       if (val.availableStocks == undefined) {
@@ -22,12 +21,13 @@ export default async function handler(req, res) {
             buCode: val.store.buCode,
           }
         }
-        logger.debug(data , debug)
         dataArr.push(data)
       }
     })
+    analytics.update("ApiUses");
     res.status(200).send(dataArr)
   } else {
+    await analytics.update("APIDisabled")
     res.status(403).send({
       message: "API is disabled",
     })
