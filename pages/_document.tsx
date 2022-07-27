cfbf32795705fb4dd2bcf0656a976be1b11c9215
devramsean0 <outramsean0@gmail.com>
2022-07-27 07:10:13 +0000

    Arrrrc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
+      <Head>
+      <script async src="https://arc.io/widget.min.js#WcxGEKCr"></script>
+      </Head>
       <Head />
       <body id="backgroundGradient">
         <Main />
