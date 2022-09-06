14b53517933b334ed9f9f4aeb686b633453040a7
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-09-06 09:18:11 +0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,13 @@ function HomePage() {
   return (
     <Layout title="Home" headerContent={(
       <>
-      <h1>Ikea Stock</h1>
+      <h1>IKEA Stock</h1>
       <form onSubmit={handleSubmit}>
       <label htmlFor="product">What is the Product ID? (Found on IKEA's Website): </label>
-      <input id="product" type="text" placeholder="here pls!" width="auto"/>
+      <input id="product" type="text" placeholder="Enter here!" width="auto"/>
       <br />
       <label htmlFor="country">What is the country you are looking at? (See the list below):</label>
-      <input id="country" type="text" placeholder="here pls!" width="auto"/>
+      <input id="country" type="text" placeholder="Enter here!" width="auto"/>
       <br />
       <button type="submit">Search</button>
     </form>
