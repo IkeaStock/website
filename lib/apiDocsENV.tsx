f572ee231642a242a2d81bee2a98e433215994c1
Sean Outram <outramsean0@gmail.com>
2022-10-27 17:58:54 +0000

    Delete all now uneeded files

--- lib/apiDocsENV.tsx
+++
@@ -1,6 +0,0 @@
-type ENV = "API_ENABLED" | "api_enabled";
-export function apiDocsENV(key: ENV) {
-    const upper = key.toUpperCase();
-    const env: any = process.env[upper];
-    return env  
-}
